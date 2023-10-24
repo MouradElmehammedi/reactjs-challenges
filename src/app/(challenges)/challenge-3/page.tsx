77ec0f1c252bb85775adeb3cfaf59200bfca8dc3
MouradElmehammedi <mouradelmehammedi@gmail.com>
2023-10-24 23:38:22 +0100

    challenge 3 : add the components Reply/Comment

--- src/app/(challenges)/challenge-3/page.tsx
+++ src/app/(challenges)/challenge-3/page.tsx
@@ -1,7 +1,142 @@
+"use client";
+import Image from "next/image";
 import React from "react";
 
+function Reply() {
+  return (
+    <>
+      <div className="flex items-center ml-10 pl-10 border-l-gray-400 border">
+        <div className="bg-white p-4 mb-4 flex-1 flex items-center space-x-4 rounded-md">
+          <div
+            style={{ backgroundColor: "hsl(223, 19%, 93%)" }}
+            className="mb-auto py-2 px-3 text-center flex flex-col gap-3 rounded-md text-gray-500 font-semibold"
+          >
+            <span>+</span>
+            <span className="text-purple-950">12</span>
+            <span>-</span>
+          </div>
+          <div className="w-full">
+            <div className="flex w-full justify-between">
+              <div className="flex space-x-4 items-center">
+                <Image
+                  src="/challenges-resources/challenge-3_interactive_comments_section/images/avatars/image-juliusomo.png"
+                  alt="avatar"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                <h1 className="text-black font-bold text-sm">amrobson</h1>
+                <span className="text-sm text-gray-400">1 mouth ago</span>
+              </div>
+              <button
+                style={{ color: "hsl(238, 40%, 52%)" }}
+                className="flex items-center gap-2 space-x-2 text-sm font-semibold outline-none px-4 py-2 rounded-md"
+              >
+                <Image
+                  src="/challenges-resources/challenge-3_interactive_comments_section/images/icon-reply.svg"
+                  alt="avatar"
+                  width={15}
+                  height={15}
+                />
+                Reply
+              </button>
+            </div>
+            <p className="mt-2 text-gray-400 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non
+              nihil dolores quidem perferendis modi corporis corrupti cum
+              assumenda, facilis sint nostrum. Quas repudiandae illo incidunt
+              laborum maiores itaque, corrupti sit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Comment() {
+  return (
+    <>
+      <div className="bg-white p-4 mb-4 flex items-center space-x-4 rounded-md">
+        <div
+          style={{ backgroundColor: "hsl(223, 19%, 93%)" }}
+          className="mb-auto py-2 px-3 text-center flex flex-col gap-3 rounded-md text-gray-500 font-semibold"
+        >
+          <span>+</span>
+          <span className="text-purple-950">12</span>
+          <span>-</span>
+        </div>
+        <div className="w-full">
+          <div className="flex w-full justify-between">
+            <div className="flex space-x-4 items-center">
+              <Image
+                src="/challenges-resources/challenge-3_interactive_comments_section/images/avatars/image-juliusomo.png"
+                alt="avatar"
+                width={30}
+                height={30}
+                className=""
+              />
+              <h1 className="text-black font-bold text-sm">amrobson</h1>
+              <span className="text-sm text-gray-400">1 mouth ago</span>
+            </div>
+            <button
+              style={{ color: "hsl(238, 40%, 52%)" }}
+              className="flex items-center gap-2 space-x-2 text-sm font-semibold outline-none px-4 py-2 rounded-md"
+            >
+              <Image
+                src="/challenges-resources/challenge-3_interactive_comments_section/images/icon-reply.svg"
+                alt="avatar"
+                width={15}
+                height={15}
+              />
+              Reply
+            </button>
+          </div>
+          <p className="mt-2 text-gray-400 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nihil
+            dolores quidem perferendis modi corporis corrupti cum assumenda,
+            facilis sint nostrum. Quas repudiandae illo incidunt laborum maiores
+            itaque, corrupti sit.
+          </p>
+        </div>
+      </div>
+      {/* other comments reply */}
+      <div className="mb-4">
+        <Reply />
+        <Reply />
+      </div>
+    </>
+  );
+}
+
 function Page() {
-  return <div>Page</div>;
+  return (
+    <main className="bg-gray-200 flex w-full min-h-screen items-center justify-center">
+      <div className="w-[600px] border p-6 rounded-lg">
+        <Comment />
+        <div className="bg-white p-4 flex items-center space-x-4 rounded-md">
+          <Image
+            src="/challenges-resources/challenge-3_interactive_comments_section/images/avatars/image-juliusomo.png"
+            alt="avatar"
+            width={30}
+            height={30}
+            className="mb-auto"
+          />
+          <textarea
+            rows={2}
+            className="flex-1 px-4 py-2 border outline-none rounded-md"
+            placeholder="Add a comment..."
+          ></textarea>
+          <button
+            style={{ backgroundColor: "hsl(238, 40%, 52%)" }}
+            className="text-sm font-semibold outline-none px-4 py-2 text-white rounded-md mb-auto"
+          >
+            SEND
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Page;
--- src/app/components/challenges.tsx
+++ src/app/components/challenges.tsx
@@ -1,31 +1,43 @@
-
-import Link from 'next/link'
-import Image from 'next/image'
-import { challengesData } from '../data'
+import Link from "next/link";
+import Image from "next/image";
+import { challengesData } from "../data";
 
 function Challenges() {
   return (
-    <div className='grid md:grid-cols-3 px-12 gap-3 mx-auto pt-10 border-t'>
-      {challengesData && challengesData.map((challenge, index) =>
-        <Link key={index} href={challenge.href} className="px-6 py-4 mx-auto border rounded-lg overflow-hidden shadow-xl text-left">
-          <div className="">
-            <div className="text-lg mb-4">{challenge.name}</div>
-            <p
-              className="text-gray-700 text-xs font-light text-left"
-              dangerouslySetInnerHTML={{ __html: challenge.description }}
+    <div className="grid md:grid-cols-3 px-12 gap-3 mx-auto pt-10 border-t">
+      {challengesData &&
+        challengesData.map((challenge, index) => (
+          <Link
+            key={index}
+            href={challenge.href}
+            className="px-6 py-4 mx-auto border rounded-lg overflow-hidden shadow-xl text-left"
+          >
+            <div className="">
+              <div className="text-lg mb-4">{challenge.name}</div>
+              <p
+                className="text-gray-700 text-xs font-light text-left"
+                dangerouslySetInnerHTML={{ __html: challenge.description }}
+              />
+            </div>
+            <Image
+              src={challenge.image as string}
+              alt="image"
+              width={1000}
+              height={1000}
+              className="mt-4 shadow-lg w-[540px] h-[350px]"
             />
-          </div>
-          <Image src={challenge.image} alt='image' width={1000} height={1000} className='mt-4 shadow-lg w-[540px] h-[350px]' />
-        </Link>)
-      }
+          </Link>
+        ))}
     </div>
-  )
+  );
 }
 
-export default Challenges
+export default Challenges;
 
-{/* <div className="px-6 pt-4 pb-2">
+{
+  /* <div className="px-6 pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-      </div> */}
+      </div> */
+}
