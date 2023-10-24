9f1a304e1eb8aabe73f9ba89239375af2abce8db
MouradElmehammedi <mouradelmehammedi@gmail.com>
2023-10-24 22:45:58 +0100

    add new challenge 3

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -1,6 +1,6 @@
 export const challengesData = [
   {
-    name: 'Interactive card details form',
+    name: "Interactive card details form",
     description: `Your challenge is to build out this interactive card details form and get it looking as close to the design as possible.<br><br>
         You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.<br><br>
         Your users should be able to:<br><br>
@@ -10,11 +10,12 @@ export const challengesData = [
           - The card number, expiry date, or CVC fields are in the wrong format<br>
         - View the optimal layout depending on their device's screen size<br>
         - See hover, active, and focus states for interactive elements on the page.`,
-    href: '/challenge-1',
-    image: '/challenges-resources/challenge-1_interactive_card_details/design/desktop-design.jpg'
+    href: "/challenge-1",
+    image:
+      "/challenges-resources/challenge-1_interactive_card_details/design/desktop-design.jpg",
   },
   {
-    name: 'Results Summary Component',
+    name: "Results Summary Component",
     description: `Your challenge is building out this results summary component and getting it as close to the design as possible.<br/><br/>
     You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.<br/><br/>
     We provide the data for the results in a local data.json file. So you can use that to add the results and total score dynamically if you choose.<br/><br/>
@@ -22,7 +23,24 @@ export const challengesData = [
     - View the optimal layout for the interface depending on their device's screen size<br/>
     - See hover and focus states for all interactive elements on the page<br/>
     - Bonus: Use the local JSON data to dynamically populate the content.`,
-    href: '/challenge-2',
-    image: '/challenges-resources/challenge-2_results_summary_component/design/desktop-design.jpg'
+    href: "/challenge-2",
+    image:
+      "/challenges-resources/challenge-2_results_summary_component/design/desktop-design.jpg",
   },
-]
+  {
+    name: "Interactive Comments Section",
+    description: `Your challenge is to build out this interactive comments section and get it looking as close to the design as possible.<br><br>
+        You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.<br><br>
+        We provide the data in a local data.json file, so use that to populate the content on the first load. If you want to take it up a notch, feel free to build this as a full-stack CRUD application!<br><br>
+        Your users should be able to:<br><br>
+        - Create, Read, Update, and Delete comments and replies<br>
+        - Upvote and downvote comments<br>
+        - View the optimal layout for the app depending on the device's screen size<br>
+        - See hover states for all interactive elements on the page<br>
+        - Bonus: If you're building a purely front-end project, use localStorage to save the current state in the browser that persists when the browser is refreshed<br>
+        - Bonus: Build this project as a full-stack application`,
+    href: "/challenge-3",
+    images:
+      "/challenges-resources/challenge-3_interactive_comments_section/design/desktop-design.jpg",
+  },
+];
